73ac9a5cbba8ddf515d5066b01eb47e87fda27b5
Moises825 <moisesbecerrilalvarez313@gmail.com>
2024-10-17 18:52:05 -0600

    Paso 3: Crea una vista de inicio al estilo de una portada con tus datos

--- practicaRepaso/resources/views/welcome.blade.php
+++ practicaRepaso/resources/views/welcome.blade.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+</head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Repaso 1 LARAVEL</title>
 </head>
 <body>
-    <h1>Hola Mundo LARAVEL</h1>
-</body>
-</html>
+    <form class="" action="Repaso 1 LARAVEL" method="post" enctype="multipart/form-data">
+        <fieldset>
+        <legend>Datos</legend>
+        <label for="">Nombre: </label>
+        <input type="text" name="" value="Moises Becerril Alvarez">
+        <br>
+        <br>
+        <label for="">Apellido Paterno: </label>
+        <input type="text" name="" value="Becerril">
+        <br>
+        <br>
+        <label for="">Apellido Materno: </label>
+        <input type="text" name="" value="Alvarez">
+        <br>
+        <br>
+        <label for="">Edad: </label>
+        <input type="text" name="" value="22">
+        <br>
+        <br>
+        <label for="">Materia: </label>
+        <input type="tel" name="" value="Programación WEB">
+        <br>
+        <br>
+        <label for="">Direccion de Correo: </label>
+        <input type="email" name="" value="122042305@gmail.com">
+        <br>
+        <br>
+        <label for="">Nombre del maestro: </label>
+        <input type="text" name="" value="Iván Isay Guerra Lopéz">
+        <br>
+        <br>
