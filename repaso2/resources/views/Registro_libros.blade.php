9ffbd53cd79d144ef5b40b76f1893884aa5fe8b4
Moises825 <moisesbecerrilalvarez313@gmail.com>
2024-10-31 16:58:46 -0600

    Se creo el paso 3 de la practica repaso.

---
+++ repaso2/resources/views/Registro_libros.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Libro</title>
+</head>
+<body>
+    <h1>Registro de Libros</h1>
+    <form class="" action="EP 2 - Forularios en HTML 5.html" method="post" enctype="multipart/form-data">
+        <fieldset>
+        <legend>Datos de registro del equipo</legend>
+        <label for="">Nombre del Libro: </label>
+        <input type="text" name="" value="">
+        <br>
+        <br>
+        <label for="">Costo del libro: </label>
+        <input type="text" name="" value="">
+        <br>
+        <br>
+        <label for="">Autor: </label>
+        <input type="text" name="" value="">
+        <br>
+        <br>
+</body>
+</html>
---
+++ repaso2/resources/views/principal_libros.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Principal</title>
+</head>
+<body>
+    <h1>Pagina Principal</h1>
+    <form class="" action="EP 2 - Forularios en HTML 5.html" method="post" enctype="multipart/form-data">
+        <fieldset>
+        <legend>Datos de registro del equipo</legend>
+        <label for="">Nombre del libro: </label>
+        <input type="text" name="" value="">
+        <br>
+        <br>
+        <label for="">Editorial: </label>
+        <input type="text" name="" value="">
+        <br>
+        <br>
+        <label for="">Año del libro: </label>
+        <input type="text" name="" value="">
+        <br>
+        <br>
+        <label for="">Cant. de libros disponibles: </label>
+        <input type="text" name="" value="">
+        <br>
+        <br>
+        <label for="">Cant. de libros agotados: </label>
+        <input type="text" name="" value="21">
+        <br>
+        <br>
+</body>
+</html>
