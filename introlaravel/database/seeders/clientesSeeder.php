cb6c363ed0f30514abedb0c97cdf020383a01029
Moises825 <moisesbecerrilalvarez313@gmail.com>
2024-11-13 18:15:02 -0600

    Practica No.8  QueryBuilder: Insertar & Consultar  - Parte 1.

--- introlaravel/database/seeders/clientesSeeder.php
+++ introlaravel/database/seeders/clientesSeeder.php
@@ -12,6 +12,22 @@ class clientesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('clientes')->insert([
+            [
+                'nombre'=>'Juan',
+                'apellido'=>'Perex'
+                'correo'=>'Juan@gmail.com'
+                'telefono'=>'+1237894560'
+            ],[
+                'nombre'=>'Andrea',
+                'apellido'=>'Arredondo'
+                'correo'=>'Andrea@gmail.com'
+                'telefono'=>'+1287854560'
+            ],[
+                'nombre'=>'Moises',
+                'apellido'=>'Becerril'
+                'correo'=>'Moises@gmail.com'
+                'telefono'=>'+1239994560'
+            ]]);
     }
 }
